49c205781caeb4655426afce0869c6b780120651
Andzej Maciusovic <andzej.maciusovic@gmail.com>
2018-11-15 09:23:14 +0200

    fix formatting

--- typings/react-native-crypto.d.ts
+++ typings/react-native-crypto.d.ts
@@ -1,11 +1,11 @@
 declare module '@trackforce/react-native-crypto' {
-	interface PublicKey {
-		public: string;
-	}
+  interface PublicKey {
+    public: string;
+  }
 
-	interface KeyPair extends PublicKey {
-		private: string;
-	}
+  interface KeyPair extends PublicKey {
+    private: string;
+  }
 
   namespace Aes {
     export function encrypt(text: string, key: string, iv: string): Promise<string>;
@@ -26,13 +26,13 @@ declare module '@trackforce/react-native-crypto' {
     export function hash(password: string, saltBase64: string, iterations: number, keyLen: number, hash: 'SHA1' | 'SHA224' | 'SHA256' | 'SHA384' | 'SHA512'): Promise<string>;
   }
 
-	namespace Rsa {
-		export function generateKeys(keySize: number): Promise<KeyPair>;
-		export function encrypt(data: string, key: string): Promise<string>;
-		export function decrypt(data: string, key: string): Promise<string>;
-		export function sign(data: string, key: string, hash: 'Raw' | 'SHA1' | 'SHA224' | 'SHA256' | 'SHA384' | 'SHA512'): Promise<string>;
-		export function verify(data: string, secretToVerify: string, key: string, hash: 'Raw' | 'SHA1' | 'SHA224' | 'SHA256' | 'SHA384' | 'SHA512'): Promise<boolean>;
-	}
+  namespace Rsa {
+    export function generateKeys(keySize: number): Promise<KeyPair>;
+    export function encrypt(data: string, key: string): Promise<string>;
+    export function decrypt(data: string, key: string): Promise<string>;
+    export function sign(data: string, key: string, hash: 'Raw' | 'SHA1' | 'SHA224' | 'SHA256' | 'SHA384' | 'SHA512'): Promise<string>;
+    export function verify(data: string, secretToVerify: string, key: string, hash: 'Raw' | 'SHA1' | 'SHA224' | 'SHA256' | 'SHA384' | 'SHA512'): Promise<boolean>;
+  }
 
   export { Aes, Sha, Hmac, Pbkdf2, Rsa }
 }
