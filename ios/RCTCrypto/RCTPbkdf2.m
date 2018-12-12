022a648ab2a91bcb137c230728c6ceaaa7e4bebc
Andzej Maciusovic <andzej.maciusovic@gmail.com>
2018-12-12 15:33:02 +0200

    fix signature

--- ios/RCTCrypto/RCTPbkdf2.m
+++ ios/RCTCrypto/RCTPbkdf2.m
@@ -1,15 +1,15 @@
 #import "RCTPbkdf2.h"
-#import "Pbkdf2.h"
+#import "lib/Pbkdf2.h"
 
 @implementation RCTPbkdf2
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(hash:(NSString *)password salt:(NSString *)salt iterations:(int)iterations keyLen:(int)keyLen hash:(NSString *)hash
+RCT_EXPORT_METHOD(hash:(NSString *)password saltBase64:(NSString *)saltBase64 iterations:(int)iterations keyLen:(int)keyLen hash:(NSString *)hash
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     NSError *error = nil;
-    NSString *data = [Pbkdf2 hash:password salt:salt iterations:iterations keyLen:keyLen hash:hash];
+    NSString *data = [Pbkdf2 hash:password saltBase64:saltBase64 iterations:iterations keyLen:keyLen hash:hash];
     if (data == nil) {
         reject(@"keygen_fail", @"Key generation failed", error);
     } else {
